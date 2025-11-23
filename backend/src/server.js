6f459b79b16774ea6efbd069b9bb835d3e9ff5c7
Sarwan Maurya <166719905+Sarwan72@users.noreply.github.com>
2025-11-23 23:34:59 +0530

    cors add

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,7 +37,7 @@ const server = http.createServer(app);
 // Initialize Socket.io
 export const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://near-meet.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -107,7 +107,7 @@ io.on("connection", (socket) => {
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://near-meet.vercel.app"],
     credentials: true,
   })
 );
